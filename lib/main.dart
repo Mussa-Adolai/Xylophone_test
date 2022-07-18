f53ec81fc98f92e36016343c68955726a6b757f0
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-07-18 16:37:04 +0300

    problem.. sound does not play :missingpluginexception(no implementation found for method

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:assets_audio_player/assets_audio_player.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                final assetsAudioPlayer = AssetsAudioPlayer();
+                assetsAudioPlayer.open(
+                  Audio("assets/audio/note1.wav"),
+                );
+              },
+              child: Text('Click Me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
