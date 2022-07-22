bd5e79efab2a21fa64c431b8d636bb549168a909
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-07-22 19:37:56 +0300

    multi sounds

--- lib/main.dart
+++ lib/main.dart
@@ -3,24 +3,92 @@
 import 'package:flutter/material.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  runApp(XylophoneApp());
+}
 
 class XylophoneApp extends StatelessWidget {
+  get child => null;
+  //int soundNumber = 1;
+  play(int soundNumber) {
+    final assetsAudioPlayer = AssetsAudioPlayer();
+    assetsAudioPlayer.open(
+      Audio("assets/audio/note$soundNumber.wav"),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
-              onPressed: () {
-                final assetsAudioPlayer = AssetsAudioPlayer();
-                assetsAudioPlayer.open(
-                  Audio("assets/audio/note1.wav"),
-                );
-              },
-              child: Text('Click Me'),
-            ),
+          child: Column(
+            //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.red,
+                ),
+                onPressed: () {
+                  play(1);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                ),
+                onPressed: () {
+                  play(2);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.grey,
+                ),
+                onPressed: () {
+                  play(3);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                ),
+                onPressed: () {
+                  play(4);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.orange,
+                ),
+                onPressed: () {
+                  play(5);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.teal,
+                ),
+                onPressed: () {
+                  play(6);
+                },
+                child: child,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.purple,
+                ),
+                onPressed: () {
+                  play(7);
+                },
+                child: child,
+              ),
+            ],
           ),
         ),
       ),
